bd767e337affb8b0ce3d19b8d127adb24ffa0907
ZhangL3 <zhanglebielefeld@gmail.com>
2020-11-11 07:54:25 +0100

    lexcal scanner

--- Week 03/ast/ll.js
+++ Week 03/ast/ll.js
@@ -1,11 +1,47 @@
+/**
+ * 四则运算
+ *  词法定义
+ *      TokenNumber:
+ *          1 2 3 4 5 6 7 8 9 0 的组合
+ *      Operator: +, -, * / 之一
+ *      Whitespace: <SP>
+ *      LineTerminator: <LF><CR>
+ *  语法定义(产生式)
+ *      <Expression>::=
+ *          <AddictiveExpression><EOF>
+ * 
+ *      <AdditiveExpression>::=
+ *          <MultiplicativeExpression>
+ *          |<AdditiveExpression><+><MultiplicativeExpression>
+ *          |<AdditiveExpression><-><MultiplicativeExpression>
+ * 
+ *      <MultiplicativeExpression>::=
+ *          <Number>
+ *          |<MultiplicativeExpression><*><Number>
+ *          |<MultiplicativeExpression></><Number>
+ * 
+ *                                  Expression
+ *                                /            \
+ *                              Add             EOF(End Of File)
+ *                   /           |           \
+ *               Multi          Add+Multi     Add-Multi
+ *          /       |       \
+ *        Num   Multi*Num   Multi/Num
+ * 
+ * 
+ */
 // ()为分组捕获，匹配到其中一个()内结果时，结束此次搜索
 let regexp = /([0-9\.]+)|([ \t]+)|([\r\n]+)|(\*)|(\/)|(\+)|(\-)/g;
 
 let dictionary = ['Number', 'Whitespace', 'LineTerminator', '*', '/', '+', '-'];
 
-function tokenize(source) {
+function* tokenize(source) {
     let result = null;
+    let lastIndex = 0;
+    
     while(true) {
+        // 更新上一次找到的 index
+        lastIndex = regexp.lastIndex;
         /**
          * Note 1: exec()
          * exec 可多次对同一字符串进行查找，下一次的查找会从正则自己的 lastIndex 属性开始
@@ -16,20 +52,8 @@ function tokenize(source) {
          * index: The 0-based index of the match in the string.
          * input: The original string that was matched against.
          * 注意：即使 exec 换了另一个 string， lastIndex 也不会改变
-         */
-        result = regexp.exec(source);
-
-        if (!result) break;
-
-        for(let i = 0; i <= dictionary.length; i += 1) {
-            if(result[i]) {
-                console.log('dictionary[i - 1]: ', dictionary[i - 1]);
-            }
-        }
-        
-        console.log('result: ', result);
-        /**
-         * result:
+         * 
+         * result 的第一个值:
          * {
          *  0: '1024'
          *  1: '1024'
@@ -45,7 +69,36 @@ function tokenize(source) {
          *  length: 8
          * }
          */
+        result = regexp.exec(source);
+
+        // 如果没有找到结果，跳出
+        if(!result) break;
+        // 如果新找到的位置长于上次找到的位置，说明中间右不被识别的字符，跳出
+        if(regexp.lastIndex - lastIndex > result[0].length) break;
+
+        let token = {
+            type: null,
+            value: null,
+        };
+
+        for(let i = 0; i <= dictionary.length; i += 1) {
+            if(result[i]) {
+                // 因为 result 的第一位是找到的值，从第二位开始时分组捕获的值，所以要 i - 1
+                token.type = dictionary[i - 1];
+            }
+        }
+        token.value = result[0];
+        // 找到了一个 token，就 return 这个 token 并停下，等待下次请求
+        yield token;
+    }
+
+    // 不再有值了，输出 End Of File
+    yield {
+        type: 'EOF',
     }
 }
 
-tokenize('1024 + 10 * 25');
+// tokenize 是一个 generator 函数，所以 return 的是一个 iterator，可用 let of 调用
+for (let token of tokenize('1024 + 10 * 25')) {
+    console.log('token: ', token);
+}
