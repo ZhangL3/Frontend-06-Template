16a5bf70f9c1129d1caf8349ffba80972f8e5940
ZhangL3 <zhanglebielefeld@gmail.com>
2020-12-08 21:24:22 +0100

    Number22String

--- Week 07/2typeConvert/typeConvert.js
+++ Week 07/2typeConvert/typeConvert.js
@@ -1,37 +1,55 @@
 /**
  * str will be converted in form of notation
- * @param {string} str string of number
+ * @param {string} str string of number, begin with 0b | 0o | 0d | 0x
  * @param {number} targetNotation 2 | 8 | 10 | 16
  * @return {number}
  */
-function string2Number(str, targetNotation) {
+function StringToNumber(str, targetNotation = 10) {
     if (str[0] !== '0') return NaN;
 
     const sourceNotation = 
         str[1] === 'b' ? 2 :
         str[1] === 'o' ? 8 :
-        str[1] === 'x' ? 16 : 10;
+        str[1] === 'd' ? 10 :
+        str[1] === 'x' ? 16 : null;
 
-    console.log('sourceNotation: ', sourceNotation);
+    if (!sourceNotation) return NaN;
 
-    const sourceDec = parseInt(str, sourceNotation);
+    const sourceDec = parseInt(str.substr(2), sourceNotation);
 
     if (isNaN(sourceDec)) return NaN;
 
-    console.log('sourceDec: ', sourceDec);
-
+    // numObj.toString([radix]) 一定是数字对象(numObj)才可以利用 toString() 转进制
+    return +sourceDec.toString(targetNotation);
 }
 
 /**
  * 
  * @param {number} num number to convert to string
- * @param {number} notation 2 | 8 | 10 | 16
+ * @param {number} sourceNotation 2 | 8 | 10 | 16
+ * @param {number} targetNotation 2 | 8 | 10 | 16
  * @return {number}
  */
-function number2String(num, notation) {
+function NumberToString(num, sourceNotation = 10, targetNotation = 10) {
 
+    if (
+        sourceNotation !== 2
+        && sourceNotation !== 8
+        && sourceNotation !== 10
+        && sourceNotation !== 16
+    ) return NaN;
+
+    const pre = targetNotation === 2 ? '0b' :
+        targetNotation === 8 ? '0o' :
+        targetNotation === 10 ? '0d' :
+        targetNotation === 16 ? '0x' : null;
+
+        console.log('pre: ', pre);
+
+    if (!pre) return NaN;
+
+    return `${pre}${parseInt(num, sourceNotation).toString(targetNotation)}`;
 }
 
-console.log('string2Number(1010, 10): ', string2Number('0x1010', 10));
-
-// TODO: dec to bin, hex, oct
+console.log('string2Number(1010, 10): ', StringToNumber('0x1010', 10));
+console.log('number2String(1010, 8, 16): ', NumberToString(1010, 10, 16));
