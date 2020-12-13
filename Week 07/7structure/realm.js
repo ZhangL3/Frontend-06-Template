0b10eebcb9bec3322bec5dbf0515e24422a00727
L3 Zhang <zhanglebielefeld@gmail.com>
2020-12-13 07:47:23 +0100

    Configure Layout

--- Week 07/7structure/realm.js
+++ Week 07/7structure/realm.js
@@ -34,8 +34,8 @@ const graph = new G6.Graph({
   container: 'mountNode', // 指定挂载容器
   width: 800, // 图的宽度
   height: 500, // 图的高度
-  fitView: true,
-  fitViewPadding: [20, 40, 50, 20],
+  // fitView: true,
+  // fitViewPadding: [20, 40, 50, 20],
    // 节点在默认状态下的样式配置（style）和其他配置
    defaultNode: {
     size: 30, // 节点大小
@@ -66,6 +66,13 @@ const graph = new G6.Graph({
     labelCfg: {
       autoRotate: true, // 边上的标签文本根据边的方向旋转
     },
+  },
+  layout: {
+    // Object，可选，布局的方法及其配置项，默认为 random 布局。
+    type: 'force', // 指定为力导向布局
+    preventOverlap: true, // 防止节点重叠
+    // nodeSize: 30        // 节点大小，用于算法中防止节点重叠时的碰撞检测。由于已经在上一节的元素配置中设置了每个节点的 size 属性，则不需要在此设置 nodeSize。
+    linkDistance: 100, // 指定边距离为100
   },
 });
 
