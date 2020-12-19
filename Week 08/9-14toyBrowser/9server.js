8a79401b32a075910a7001ebb93f736cdd2cebd8
ZhangL3 <zhanglebielefeld@gmail.com>
2020-12-19 09:27:48 +0100

    buffer.from([])

--- Week 08/9-14toyBrowser/9server.js
+++ Week 08/9-14toyBrowser/9server.js
@@ -7,7 +7,7 @@ http.createServer((request, response) => {
   }).on('data', (chunk) => {
     body.push(chunk.toString())
   }).on('end', () => {
-    body = Buffer.concat(body).toString();
+    body = Buffer.concat([Buffer.from(body.toString())]).toString();
     console.log('body: ', body);
     response.writeHead(200, {'Content-Type': 'test/html'});
     response.end('Hello World\n');
