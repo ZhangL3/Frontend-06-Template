a0982a1cc7f0788378bf889e9453acf938f7d02a
L3 Zhang <zhanglebielefeld@gmail.com>
2021-01-30 09:44:44 +0100

    config webpack-dev-server

--- Week 14/Carousel/webpack.config.js
+++ Week 14/Carousel/webpack.config.js
@@ -1,7 +1,15 @@
 const { Module } = require("webpack");
+const path = require('path');
 
 module.exports = {
   entry: "./main.js",
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+  },
   module: {
     rules: [
       {
