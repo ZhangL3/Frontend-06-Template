03358bc213007c9a44a7e9e69e69958e36305c22
L3 Zhang <zhanglebielefeld@gmail.com>
2021-02-21 14:35:33 +0100

    nyc without babel

--- Week 18/test-demo/add.js
+++ Week 18/test-demo/add.js
@@ -1,5 +1,11 @@
-export function add(a, b) {
+// export function add(a, b) {
+ function add(a, b) {
   return a + b;
 }
 
-// module.exports = add;
+function mul(a, b) {
+  return a * b;
+}
+
+module.exports.add = add;
+module.exports.mul = mul;
--- Week 18/test-demo/test/test.js
+++ Week 18/test-demo/test/test.js
@@ -1,6 +1,7 @@
 var assert = require('assert');
-import { add } from '../add';
-// var add = require('../add');
+// import { add } from '../add';
+var add = require('../add').add;
+var mul = require('../add').mul;
 
 describe("add function testing", function () {
   it('1 + 2 should be 3', function () {
@@ -9,5 +10,9 @@ describe("add function testing", function () {
 
   it('-5 + 2 should be -3', function () {
     assert.strictEqual(add(-5, 2), -3);
+  });
+
+  it('-5 * 2 should be -10', function () {
+    assert.strictEqual(mul(-5, 2), -10);
   });
 })
