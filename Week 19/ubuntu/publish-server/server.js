0993a40986daf07e82514f5f92708ee799764466
L3 Zhang <zhanglebielefeld@gmail.com>
2021-03-01 22:50:16 +0100

    Get code, can not get token

--- Week 19/ubuntu/publish-server/server.js
+++ Week 19/ubuntu/publish-server/server.js
@@ -1,9 +1,47 @@
 let http = require('http');
+let https = require('https');
 // let fs = require('fs');
 let unzipper = require('unzipper');
+let querystring = require('querystring');
+const { hostname } = require('os');
+
+// 2. auth 路由：接收 code，用 code + client_id + client_secret 换 token
+function auth(request, response) {
+  let query = querystring.parse(request.url.match(/^\/auth\?([\s\S]+)$/)[1]);
+  console.log('query: ', query.code);
+  getToken(query.code);
+}
+
+function getToken(code) {
+  let request = https.request({
+    hostname: "github.com",
+    path: `/login/oauth/access_token?code=${code}&client_id=Iv1.f71dba4980135246&client_secret=6d2682b27220fbea5516e07a4bdc4d1e921c1ad7`,
+    port: 443,
+    method: "POST",
+  }, function(response) {
+    console.log('response: ', response);
+    // response.on('data', chunk => {
+    //   console.log('!!!!!! chunk: ', chunk.toString());
+    //   return;
+    // })
+  });
+  request.end();
+}
+
+// 4. publish 路由: 用 token 获取用户信息, 检查权限,接受发布
+function publish(request, response) {
+
+}
 
 http.createServer(function (request, response) {
-  console.log('request.headers: ', request.headers);
+  // request.url: http://localhost:8882/auth?code=3d03cf2cf054fea6a484
+  if (request.url.match(/^\/auth\?/)) {
+    return auth(request, response);
+  }
+  if (request.url.match(/^\/publish\?/)) {
+    return publish(request, response);
+  }
+  // console.log('request.headers: ', request.headers);
 
   // let outFile = fs.createWriteStream("../server/public/tmp.zip");
   // request.pipe(outFile);
--- Week 19/windows/publish-tool/publish.js
+++ Week 19/windows/publish-tool/publish.js
@@ -1,9 +1,20 @@
 let http = require('http');
 let fs = require("fs");
 let archiver = require("archiver");
+let child_process = require("child_process")
 
 // let file = fs.createReadStream("./sample.html");
 let request;
+
+// 1. 打开 https://github.com/login/oauth/authorize
+
+child_process.exec(
+  `start https://github.com/login/oauth/authorize?client_id=Iv1.f71dba4980135246`, (err) => {
+  console.log('err: ', err);
+});
+
+
+// 3. 创建 server，接受 token，然后点击发布
 
 // request to locolhost
 // let request = http.request({
@@ -48,32 +59,32 @@ let request;
 //   })
 // })
 
-// multi files
-
-request = http.request({
-  hostname: "192.168.177.230",
-  port: 8882,
-  method: "post",
-  headers: {
-    'Content-Type': 'application/octet-stream',
-  }
-}, response => {
-  console.log('response: ', response);
-})
-
-// file.pipe(request);
-
-// 压缩文件夹并 pipe 给 request
-const archive = archiver('zip', {
-  zlib: {level: 9}
-});
-
-archive.directory('./sample/', false);
-archive.finalize();
-// archive.pipe(fs.createWriteStream("tmp.zip"));
-archive.pipe(request);
-
 // file.on('end', chunk => {
 //   console.log('read finished: ', chunk);
 //   request.end();
 // })
+
+// multi files
+
+// request = http.request({
+//   hostname: "192.168.177.230",
+//   port: 8882,
+//   method: "post",
+//   headers: {
+//     'Content-Type': 'application/octet-stream',
+//   }
+// }, response => {
+//   console.log('response: ', response);
+// })
+
+// // file.pipe(request);
+
+// // 压缩文件夹并 pipe 给 request
+// const archive = archiver('zip', {
+//   zlib: {level: 9}
+// });
+
+// archive.directory('./sample/', false);
+// archive.finalize();
+// // archive.pipe(fs.createWriteStream("tmp.zip"));
+// archive.pipe(request);
